6c0e7563816544bf6affc4604953f3877eabf327
sarahkumw <sarahwarren3892@gmail.com>
2022-10-22 00:11:02 -0700

    refactored and commented code

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//global variables
 let number1 = [];
 let number2 = [];
 let operation;
 let symbol;
 
 
-
+// operation functions
 function addition (num1, num2) {
     return num1 + num2;
 };
@@ -21,48 +22,43 @@ function division (num1, num2) {
     return num1 / num2;
 };
 
+//called when user presses "="
 function calculate() {
+    //if there is no second number, just return first number
     if(number2.length === 0){
         const answer = +(number1).join("");
         number1 = Array.from(String(answer));
         number2 = [];
-        const parentNode = document.querySelector("#display");
-        parentNode.removeChild(parentNode.firstChild);
-        const calcDisplay = document.createElement("p");
-        parentNode.appendChild(calcDisplay);
-        calcDisplay.textContent = `${answer}`
+        const display = document.querySelector("#display");
+        display.textContent = `${answer}`
     }
+    //if user tries to calculate number / 0
     else if (number2.length === 1 && number2[0] === 0 && operation === division){  
         return alert("Can't divide by 0!")
     }
     else {
+        //join number1 and number2 arrays into numbers and perform operation
         let num1 = +(number1).join("");
         let num2 = +(number2).join("");
         let answer = operation(num1, num2);
+        //if answer has a decimal and more than 5 decimal places, round to 5 decimal places
         if(answer % 1 != 0) {
             if(answer.toString().split(".")[1].length > 5){
-                console.log(`answer has ${answer.toString().split(".")[1].length} decimal places`)
                 answer = answer.toFixed(5);
-                console.log(`rounded answer is ${answer}`)
             }
         }
+        //set number1 to array from answer so user can perform the next operation on it, 
+        //and clear number2
         number1 = Array.from(String(answer));
         number2 = [];
-        const parentNode = document.querySelector("#display");
-        parentNode.removeChild(parentNode.firstChild);
-        const calcDisplay = document.createElement("p");
-        parentNode.appendChild(calcDisplay);
-        calcDisplay.textContent = `${answer}`
+        const display = document.querySelector("#display");
+        display.textContent = `${answer}`
     }
 };
 
+//called every time display updates when buttons are pushed
 function display() {
-    const parentNode = document.querySelector("#display");
-    if(parentNode.firstChild){
-        parentNode.removeChild(parentNode.firstChild);
-    }
-    const calcDisplay = document.createElement("p");
-    parentNode.appendChild(calcDisplay)
+    const display = document.querySelector("#display");
     if (!symbol) {
         symbol = "";
     }
@@ -80,9 +76,11 @@ function display() {
     else {
         num2 = "";
     }
-    calcDisplay.textContent = `${num1} ${symbol} ${num2}`
+    display.textContent = `${num1} ${symbol} ${num2}`
 }
 
+//pushes number into number1 or number2 array depending on if the operation has been selected yet
+//doesn't allow decimal to be pushed into a number twice
 function numberButton(number) {
     if(operation) {
         if(number === "." && number2.includes(".")){
@@ -104,6 +102,7 @@ function numberButton(number) {
     }
 }
 
+//sets the operation to be performed and symbol to be displayed
 function operationButton(operator, operand) {
     if(operation){
         calculate();
@@ -118,6 +117,7 @@ function operationButton(operator, operand) {
     }
 }
 
+//pops off one element of either number array or removes operation
 function backspace() {
     if(number2.length > 0) {
         number2.pop();
@@ -132,11 +132,12 @@ function backspace() {
     display()
 }
 
+//resets global variables and clears display element
 function clearDisplay() {
     number1 = [];
     number2 = [];
     symbol = null;
     operation = null;
-    const parentNode = document.querySelector("#display");
-    parentNode.removeChild(parentNode.firstChild);
-}
+    const display = document.querySelector("#display");
+    display.textContent = "";
+};
